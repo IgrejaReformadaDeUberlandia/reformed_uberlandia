00c76eaed6804ec34576e4de47f0593af94192ea
muellerviana <muellerviana@gmail.com>
2024-12-25 17:02:13 -0300

    create site home index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :site do
+    get 'about/index'
     get 'home/index'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -9,5 +10,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "site/home#index"
 end
