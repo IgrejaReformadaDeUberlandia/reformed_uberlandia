64690ac8321a7504306adc27beaf123593f8499e
muellerviana <muellerviana@gmail.com>
2025-01-02 17:06:29 -0300

    update topbar with oficial design

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,8 +9,25 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#485872',
+        secondary: {
+          100: '#f7ebe6',
+          200: '#bcb3af',
+        },
+        terciary: '#a98558',
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      animation: {
+        down: 'down 1.3s ease-out',
+      },
+      keyframes: {
+        down: {
+          '0%': { opacity: '0', transform: 'translateY(-50px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
       },
     },
   },
